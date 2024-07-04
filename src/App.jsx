622ebaa930d42d853fa8896f66cc821afac167fe
Bappi Das <151029855+bappidass@users.noreply.github.com>
2024-07-04 18:14:56 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState('reac');
+  const [btnname,setbtnname]=useState('ON');
+  
+  function start(PROPS){
+   
+    if(PROPS=='ON'){
+      setCount('react')
+      setbtnname('OFF')
+    }else{
+      setCount('reac')
+      setbtnname('ON')
+    }
+   
+  }
+
+  return (
+    <>
+      <div>
+       <div>
+        <a href="" target="_blank">
+          <img src='fan.png' className={count} alt="React logo" />
+        </a>
+       </div>
+       <div>
+        <audio controls autoPlay loop >
+         <source  src="garam.mp3" type="audio/ogg"/>
+        </audio>
+       </div>
+       <div className="bottons">
+         <button onClick={()=>{btnname=='ON' ? start(btnname) : start(btnname) }}>{btnname}</button>
+         <button onClick={()=>{btnname=='OFF' ?setCount('react1'):alert('First on fan')}}>1</button>
+         <button onClick={()=>{btnname=='OFF' ?setCount('react2'):alert('First on fan')}} >2</button>
+       </div>
+      </div>
+      <div className="madeby">
+        <p>Made by bappi</p>
+      </div>
+    </>
+  )
+}
+
+export default App
